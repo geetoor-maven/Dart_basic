e0f4134a8978a4f3ecbce61971a62d5f45f50889
shegi-developer <agus40994@gmail.com>
2020-09-25 08:31:05 +0800

    Dart Basic

---
+++ Tipedata.dart
@@ -0,0 +1,25 @@
+void main() {
+  
+  // Tipe data String 
+  String nama;
+  // Tipe data int
+  int umur;
+  // Tipe data boolean
+  bool status;
+  // tipe data double
+  double nilai;
+
+
+  // memberikan nilai pada tipe data sesuai dengan kewajiban nilai yang harus di milikinya 
+  nama = 'Agus Kurniawan';
+  umur = 21;
+  status = false;
+  nilai = 90.0;
+
+  // memunculkan semua tipe data beserta nilainya
+  print('Nama saya = '+nama);
+  print('Umur saya = '+umur.toString());
+  print('Status menikah saya = '+ status.toString());
+  print('Nilai terakhir saya = '+nilai.toString());
+
+}
